aa1864cdeeef2564b72f1eaea10d57942b87f06d
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-06-17 21:36:12 +0530

    [somu]Add:Deleting Element Using Generic Class Concept.

--- Day5PracticeProblems/DeleteArray.cs
+++ Day5PracticeProblems/DeleteArray.cs
@@ -7,62 +7,34 @@ using System.Threading.Tasks;
 
 namespace Day5PracticeProblems
 {
-    public class DeleteArray
+    public class DeleteArray<T> where T : IComparable
     {
-        int[] intarray = { 1, 3, 6, 4, 7, 8, 10, 2, 5, 9 };
-        double[] doublearray = { 1.3, 3.3, 6.7, 4.8, 7.3, 8.2, 10.9, 2.2, 5.4, 9.6 };
-        char[] chararray = { 'a', 'b', 'c', 'g', 'f', 'e' };
-        public void DeleteUsingGenerics<T>(T[] arr, T search) where T : IComparable
+        T[] array;
+        public DeleteArray(T[] array) 
+        { 
+            this.array = array;
+        }
+        public void DeleteUsingGenerics(T search)
         {
             T[] arr1 = new T[10];
             int k = 0;
             int flag = 0;
-            for (int i = 0; i < arr.Length; i++)
+            for (int i = 0; i < array.Length; i++)
             {
-                if (arr[i].Equals(search))
+                if (array[i].Equals(search))
                 {
                     flag = 1;
                 }
                 if (flag == 0)
                 {
-                    arr1[k] = arr[i];
+                    arr1[k] = array[i];
                     k++;
                 }
             }
-            arr = arr1;
+            array = arr1;
             for (int i = 0; i < k; i++)
             {
-                Console.WriteLine(arr[i]);
-            }
-        }
-        public void DeleteElementInArray()
-        {
-            bool flag = true;
-            while (flag)
-            {
-                Console.WriteLine("Select Array to delete Element \n 1. IntArray \n 2. FloatArray \n 3. CharArray \n 4. Exit ");
-                int option = Convert.ToInt32(Console.ReadLine());
-                switch (option)
-                {
-                    case 1:
-                        Console.WriteLine("Enter the element you want to delete");
-                        int search = Convert.ToInt32(Console.ReadLine());
-                        DeleteUsingGenerics<int>(intarray, search);
-                        break;
-                    case 2: 
-                        Console.WriteLine("Enter the element you want to delete");
-                        double search1 = Convert.ToDouble(Console.ReadLine());
-                        DeleteUsingGenerics<double>(doublearray, search1);
-                        break;
-                    case 3:
-                        Console.WriteLine("Enter the element you want to delete");
-                        char search2 = Convert.ToChar(Console.ReadLine());
-                        DeleteUsingGenerics<char>(chararray, search2);
-                        break;
-                    default:
-                        flag = false;
-                        break;
-                }
+                Console.WriteLine(array[i]);
             }
         }
     }
--- Day5PracticeProblems/Program.cs
+++ Day5PracticeProblems/Program.cs
@@ -4,8 +4,15 @@
     {
         public static void Main(string[] args)
         {
-            DeleteArray deleteArray = new DeleteArray();
-            deleteArray.DeleteElementInArray();
+            int[] IntArray = { 1, 5, 8, 9, 6, 3, 4 };
+            double[] DoubleArray = { 1.1, 5.7, 9.8, 5.6 };
+            char[] CharArray = { 'a', 'b', 'c', 'd', 'e', 'f' };
+            DeleteArray<int> deleteArray = new DeleteArray<int>(IntArray);
+            deleteArray.DeleteUsingGenerics(5);
+            DeleteArray<double> deleteArray1 = new DeleteArray<double>(DoubleArray);
+            deleteArray1.DeleteUsingGenerics(9.8);
+            DeleteArray<char> deleteArray2 = new DeleteArray<char>(CharArray);
+            deleteArray2.DeleteUsingGenerics('d');
         }
     }
 }
